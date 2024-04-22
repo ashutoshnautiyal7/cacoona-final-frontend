423de76a38121e08846cc9b0f6e5687317362897
AyushChamoli961 <ayushchamoli001@gmail.com>
2024-04-23 00:41:29 +0530

    productId changed

--- components/Home/Product.jsx
+++ components/Home/Product.jsx
@@ -40,7 +40,7 @@ const Product = ({
         <span className="px-2 py-2 rounded-full text-white text-[12px] bg-[#4FA2AE] absolute top-3 right-3">
           <FaRegHeart className="h-4 w-4" />
         </span>
-        <AddToBag id={prodcutId} imageSrc={imageSrc} productName={productName} currentPrice={currentPrice} originalPrice={originalPrice} discount={discount} rating={rating} totalRatings={totalRatings}
+        <AddToBag id={Id} imageSrc={imageSrc} productName={productName} currentPrice={currentPrice} originalPrice={originalPrice} discount={discount} rating={rating} totalRatings={totalRatings}
         />
       </div>
 
