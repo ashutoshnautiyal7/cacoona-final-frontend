06a0d50c9ca824d21e06593727a2469a4e20ce01
Ashutosh <ashutoshnautiyal94@gmail.com>
2024-05-05 01:46:51 +0530

    no mods

--- components/category/Category.jsx
+++ components/category/Category.jsx
@@ -5,30 +5,27 @@ import Product from "../Home/Product";
 
 const Category = ({ productList }) => {
   return (
-    
-     
-      
-        <div
-          className="mt-8 md:mt-10 flex flex-wrap justify-center md:justify-start gap-6 md:gap-10"
-          style={{ scrollbarWidth: "none" }}
-        >
-          {productList.map((product) => (
-            <Product
-              key={product.id}
-              Id={product.id}
-              imageSrc={product.images[0].url}
-              productName={product.name}
-              currentPrice={product.currentPrice}
-              originalPrice={product.originalPrice}
-              discount={product.category}
-              rating={4.5}
-              totalRatings={5}
-            />
-          ))}
-        </div>
-     
-   
+    <div
+      className="mt-8 md:mt-10 flex flex-wrap justify-center md:justify-start gap-6 md:gap-10"
+      style={{ scrollbarWidth: "none" }}
+    >
+      {productList.map((product) => (
+        <Product
+          key={product.id}
+          Id={product.id}
+          imageSrc={product.images[0].url}
+          productName={product.name}
+          currentPrice={product.currentPrice}
+          originalPrice={product.originalPrice}
+          discount={product.category}
+          rating={4.5}
+          totalRatings={5}
+        />
+      ))}
+    </div>
   );
 };
+
+// just adding this for rebuild
 
 export default Category;
