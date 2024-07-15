4774402bdb388fe04bae0fe7cc4e5b9b526f6f50
Ashutosh <ashutoshnautiyal94@gmail.com>
2024-07-15 22:14:53 +0530

    minor fixes done

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
-// import Footer from "@/components/landing/Footer";
-// import Footer from "@/components/Footer/Footer";
+
 import Footer2 from "@/components/Footer/Footer2";
 import Hero from "@/components/Home/Hero";
 import Section1 from "@/components/Home/Section1";
@@ -28,16 +27,13 @@ export default async function Home() {
 
   return (
     <main className="">
-      {/* <Hero />
-      <Section1 />
-      <Section2 /> */}
+     
       <Navbar />
       <Hero />
-      <Section1 productList={products} timer={timer}/>
+      <Section1 productList={products} timer={timer} />
       <Section2 />
       <Section3 productList={products} />
       <Section4 timer = {timer}/>
-      {/* <Section5 productList={products}/>  */}
       <Section6 />
       <Section7/>
       <Footer2 />
